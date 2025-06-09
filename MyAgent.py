a5ba896a8827f2c9c0de2c7c28626c7b15253f85
venurao983 <106814802+venurao983@users.noreply.github.com>
2025-06-08 22:12:50 -0500

    Update MyAgent.py

--- MyAgent.py
+++ MyAgent.py
@@ -1,59 +1,97 @@
+import pickle
 import random
+import math
 
 class Agent:
+    def __init__(self):
+        self._Qvalues = {}
+        self._values = {}
+
+        self._learningRate = 0.1
+        self._discountFactor = 0.95
+        self._epsilon = 0.2  # exploration rate
+
+        # Discretization settings
+        self._numBins = 5  # number of bins per lidar sensor
+        self._velocityBins = 3
+
+    
+    import math
+
+    def getBin(self, observation):
+        bins = []
+
+        for d in observation:
+            try:
+                d = float(d)  # convert to float in case it's a string
+            except ValueError:
+                d = 0.0  # default to 0.0 if conversion fails
+
+            if math.isinf(d):
+                d = 1.0  # treat infinite as max distance
+
+            d = min(max(d, 0.0), 1.0)  # clamp to [0.0, 1.0]
+
+            bins.append(min(int(d * self._numBins), self._numBins - 1))
+
+        return tuple(bins)
+
+
     def chooseAction(self, observations, possibleActions):
-        lidar = observations['lidar']
-        velocity = observations['velocity']
+        state = self.getBin(observations)
 
-        left = lidar[0]
-        front_left = lidar[1]
-        front = lidar[2]
-        front_right = lidar[3]
-        right = lidar[4]
+        # Epsilon-greedy policy
+        if random.random() < self._epsilon:
+            return random.choice(possibleActions)
 
-        front_limit= 0.9
-        side_limit = 0.9
-        corner_limit = 0.9
-        low_vel = 0.05
-        normal_speed = 0.4
+        # Pick best action
+        bestAction = possibleActions[0]
+        bestValue = self._Qvalues.get(state + (bestAction,), 0)
+        for action in possibleActions:
+            q = self._Qvalues.get(state + (action,), 0)
+            if q > bestValue:
+                bestValue = q
+                bestAction = action
+        return bestAction
 
-        if lidar.count(float('inf')) == len(lidar):
-            return ('straight', 'accelerate')
+    def updateValues(self, observation1, action, observation2, reward):
+        state1 = self.getBin(observation1)
+        state2 = self.getBin(observation2)
 
-        if velocity < low_vel:
-            return ('straight', 'accelerate')
+        oldQ = self._Qvalues.get(state1 + (action,), 0)
+        nextValue = max([self._Qvalues.get(state2 + (a,), 0) for a in [('left','accelerate'), ('left', 'coast'), ('left','brake'),
+                                                                      ('straight','accelerate'), ('straight', 'coast'), ('straight','brake'),
+                                                                      ('right','accelerate'), ('right', 'coast'), ('right','brake')]])
 
-        if left < side_limit:
-            return ('right', 'brake')
+        newQ = (1 - self._learningRate) * oldQ + self._learningRate * (reward + self._discountFactor * nextValue)
+        self._Qvalues[state1 + (action,)] = newQ
+        self._values[state1] = max([self._Qvalues.get(state1 + (a,), 0) for a in [('left','accelerate'), ('left', 'coast'), ('left','brake'),
+                                                                                 ('straight','accelerate'), ('straight', 'coast'), ('straight','brake'),
+                                                                                 ('right','accelerate'), ('right', 'coast'), ('right','brake')]])
 
-        if right < side_limit:
-            return ('left', 'brake')
+    def train(self, repetitions):
+        from Track import buildTrack
+        from Racecar import Racecar
 
-        if front < front_limit:
-            if left > right:
-                return ('left', 'brake')
-            else:
-                return ('right', 'brake')
+        for _ in range(repetitions):
+            track = buildTrack(random.randint(1, 8))
+            car = Racecar(track)
+            obs, _, done = car.step(('straight','coast'))
 
-        if front_left < corner_limit:
-            return ('right', 'brake')
+            steps = 0
+            while not done and steps < 1000:
+                steps += 1
+                action = self.chooseAction(obs, car.actions())
+                nextObs, reward, done = car.step(action)
+                self.updateValues(obs, action, nextObs, reward)
+                obs = nextObs
 
-        if front_right < corner_limit:
-            return ('left', 'brake')
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self._values, f)
+            pickle.dump(self._Qvalues, f)
 
-        if left < side_limit:
-            return ('right', 'coast')
-
-        if right < side_limit:
-            return ('left', 'coast')
-
-        if front < front_limit:
-            if front_left < front_right:
-                return ('right', 'accelerate')
-            else:
-                return ('left', 'accelerate')
-
-        if velocity < normal_speed:
-            return ('straight', 'accelerate')
-
-        return ('straight', 'coast')
+    def load(self, filename):
+        with open(filename, 'rb') as f:
+            self._values = pickle.load(f)
+            self._Qvalues = pickle.load(f)
